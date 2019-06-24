f2a058b0d0e5c6e768f4016964c577ebb3172722
alexanderrobitzsch <robitzsch@ipn.uni-kiel.de>
2019-06-24 10:07:13 +0200

    3.6-4

--- R/lsem_bootstrap_inference.R
+++ R/lsem_bootstrap_inference.R
@@ -1,11 +1,11 @@
 ## File Name: lsem_bootstrap_inference.R
-## File Version: 0.03
+## File Version: 0.04
 
 lsem_bootstrap_inference <- function(parameters_boot, est)
 {
     R <- ncol(parameters_boot)
     est_boot <- rowMeans(parameters_boot, na.rm=TRUE)
-    se_boot <- sqrt( rowMeans( ( parameters_boot - est_boot )^2 ) / (R-1) )
+    se_boot <- sqrt( rowSums( ( parameters_boot - est_boot )^2 ) / (R-1) )
     bias_boot <- est_boot - est
     est_bc <- est - bias_boot
     #-- output
